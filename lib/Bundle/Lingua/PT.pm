9543d8d407afff41074b3770f9a6c328bafc2864
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2004-12-24 16:10:57 +0000

    Import of AMBS/Bundle-Lingua-PT-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Lingua-PT
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AMBS/Bundle/Bundle-Lingua-PT-0.01.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Lingua/PT.pm
@@ -0,0 +1,57 @@
+package Bundle::Lingua::PT;
+
+$VERSION = '0.01';
+
+=head1 NAME
+
+Bundle::Lingua::PT - install a set of useful modules for Natural
+Language Processing of the Portuguese
+
+=head1 SYNOPSIS
+
+cpan Bundle::Lingua::PT
+
+=head1 CONTENTS
+
+Text::Ngram
+
+Text::Affixes
+
+Lingua::PT::Abbrev
+
+Lingua::PT::Conjugate
+
+Lingua::PT::Hyphenate
+
+Lingua::PT::Inflect
+
+Lingua::PT::Nums2Ords
+
+Lingua::PT::Nums2Words
+
+Lingua::PT::Ords2Nums
+
+Lingua::PT::PLN
+
+Lingua::PT::PLNbase
+
+Lingua::PT::ProperNames
+
+Lingua::PT::Words2Nums
+
+=head1 AUTHOR
+
+Alberto Simões, C<< <ambs@cpan.org> >>
+
+José Castro, C<< <cog@cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2004 Alberto Simões and José Castro, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of Bundle::Lingua::PT
---
+++ t/00.load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'Bundle::Lingua::PT' );
+}
+
+diag( "Testing Bundle::Lingua::PT $Bundle::Lingua::PT::VERSION" );
