7804ccffff45f1510a916499c24f76774427ca41
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2005-08-12 18:37:43 +0000

    Import of AMBS/Bundle-Lingua-PT-0.02 from CPAN.
    gitpan-cpan-distribution: Bundle-Lingua-PT
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         AMBS/Bundle/Bundle-Lingua-PT-0.02.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

--- lib/Bundle/Lingua/PT.pm
+++ lib/Bundle/Lingua/PT.pm
@@ -1,6 +1,6 @@
 package Bundle::Lingua::PT;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 =head1 NAME
 
@@ -11,11 +11,21 @@ Language Processing of the Portuguese
 
 cpan Bundle::Lingua::PT
 
+=head1 DESCRIPTION
+
+This module includes some specific modules related to Natural Language
+Processing of the Portuguese language, as well as some others we find
+useful to have when doing this kind of task.
+
+If you think some useful module is missing let us know.
+
 =head1 CONTENTS
 
 Text::Ngram
 
 Text::Affixes
+
+Lingua::Identify
 
 Lingua::PT::Abbrev
 
@@ -39,6 +49,8 @@ Lingua::PT::ProperNames
 
 Lingua::PT::Words2Nums
 
+Lingua::PT::Speaker
+
 =head1 AUTHOR
 
 Alberto Simões, C<< <ambs@cpan.org> >>
@@ -47,7 +59,7 @@ José Castro, C<< <cog@cpan.org> >>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2004 Alberto Simões and José Castro, All Rights Reserved.
+Copyright 2005 Alberto Simões and José Castro, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
